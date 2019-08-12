2e17ebdf6fef48a5b438c0bc162a2c8afe4c5548
guoruixiao <grx0917@sina.com>
2019-08-12 18:55:56 +0800

    Initial commit

---
+++ SDTrackTool_Demo/SDTrackTool_Demo/SDTrackTool/SDTrackTool.m
@@ -0,0 +1,45 @@
+//
+//  SDTrackTool.m
+//  sdtrack
+//
+//  Created by lisd on 2017/4/26.
+//  Copyright © 2017年 kingnet. All rights reserved.
+//
+
+#import "SDTrackTool.h"
+//#import "UMMobClick/MobClick.h"
+
+@implementation SDTrackTool
+
++ (void)configure {
+//    UMConfigInstance.appKey = UMENG_KEY;
+//    UMConfigInstance.eSType = E_UM_NORMAL;
+////    UMConfigInstance.ePolicy = 0;
+//    NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
+//    NSString *currentVersion = [infoDict objectForKey:@"CFBundleShortVersionString"];
+//    [MobClick setAppVersion:currentVersion];
+//    [MobClick startWithConfigure:UMConfigInstance];
+//
+//#ifdef DEBUG
+//    [MobClick setLogEnabled:YES];
+//    [MobClick setCrashReportEnabled:NO];
+//#endif
+}
+
++(void)beginLogPageID:(NSString *)pageID {
+//    [MobClick beginLogPageView:pageID];
+    NSLog(@"进入页面============%@",pageID);
+}
+
++(void)endLogPageID:(NSString *)pageID {
+//    [MobClick endLogPageView:pageID];
+    NSLog(@"离开页面============%@",pageID);
+
+}
+
++(void)logEvent:(NSString*)eventId {
+//    [MobClick event:eventId];
+    NSLog(@"Targit-Action============%@",eventId);
+}
+
+@end
